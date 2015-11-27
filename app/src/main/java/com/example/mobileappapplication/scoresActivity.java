aeabf3b515db4d63aa0349aba8b6f69633c04649
hoiling <lingx0987@gmail.com>
2015-11-28 03:52:17 +0800

    second commit

--- app/src/main/java/com/example/mobileappapplication/scoresActivity.java
+++ app/src/main/java/com/example/mobileappapplication/scoresActivity.java
@@ -20,14 +20,14 @@ public class scoresActivity extends Activity {
     static final String TABLE_NAME = "GameRank";
     static final String[] COLUMN = {"name", "scores"};
 
+    TextView data = (TextView)findViewById(R.id.scoresResult);
+    Button backButton = (Button)findViewById(R.id.scoresBackButton);
+
     public Toast toast;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
-
-        TextView data = (TextView)findViewById(R.id.scoresResult);
-        Button backButton = (Button)findViewById(R.id.backButton);
 
         try {
             db = openOrCreateDatabase(DATABASE_NAME, SQLiteDatabase.CREATE_IF_NECESSARY, null);
@@ -39,12 +39,23 @@ public class scoresActivity extends Activity {
 
         String sql = "select * from " + TABLE_NAME + "order by " + COLUMN[1];
         Cursor cursor = db.rawQuery(sql, null);
-
+        String result = "";
+        while(cursor.moveToNext()) {
+            int count = 1;
+            String name = cursor.getString(cursor.getColumnIndex(COLUMN[0]));
+            int score = cursor.getInt(cursor.getColumnIndex(COLUMN[1]));
+            result += Integer.toString(count) + ". " + name + " " + Integer.toString(score);
+            count++;
+        }
+        data.setText(result);
+        db.close();
     }
 
     public void backMethod(View v) {
         Intent intent = new Intent();
         intent.setClassName(this, getPackageName() + "." + "startActivity");
+        startActivity(intent);
+        data.setText("");
 
     }
 
